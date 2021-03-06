f84a0407dfff383d8a496deb763ae8b1b8a35678
Vladislav Titov <65026443+VladiTitov@users.noreply.github.com>
2021-03-06 22:47:51 +0300

    first commit

---
+++ currencyExchangeTelegramBOT/currencyExchangeTelegramBOT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace currencyExchangeTelegramBOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
