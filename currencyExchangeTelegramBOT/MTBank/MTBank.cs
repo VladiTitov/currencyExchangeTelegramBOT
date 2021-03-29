08916a8fe812ee091008386f0cc449f84907063c
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-03-29 17:53:14 +0300

    добавлен MTBank

---
+++ currencyExchangeTelegramBOT/MTBank/MTBank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTBank
+{
+    class MTBank
+    {
+        private string URL;
+        private string cacheless;
+
+        public MTBank(string _url)
+        {
+            URL = _url;
+        }
+
+        public string Code { get; set; }
+        public string CodeTo { get; set; }
+        public string Purchase { get; set; }
+        public string Sale { get; set; }
+        public bool Cacheless { get; set; }
+        
+    }
+}
