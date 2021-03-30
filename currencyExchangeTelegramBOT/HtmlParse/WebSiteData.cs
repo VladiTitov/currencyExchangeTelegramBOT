a9a6596d3d5582d52a21d86dcecfa61b987aaa5a
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-03-30 17:35:27 +0300

    создаю парсер для сайта с данными

---
+++ currencyExchangeTelegramBOT/HtmlParse/WebSiteData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace HtmlParse
+{
+    class WebSiteData
+    {
+        //USD = https://select.by/kurs-dollara
+        //EUR = https://select.by/kurs-evro
+        //RUB = https://select.by/kurs-rublya
+
+        public WebSiteData(string Part)
+        {
+            List<string> Addr = new List<string>();
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = @"https://select.by/kurs-" + $"{Part}";
+        }
+
+    }
+
+    
+}
