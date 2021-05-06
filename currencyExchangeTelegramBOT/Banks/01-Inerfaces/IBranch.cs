1a837f418fc26c3cf17f6ef4deacc9ce0d2f5703
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-05-06 14:18:39 +0300

    создана сущность котировок валют

--- currencyExchangeTelegramBOT/Banks/01-Inerfaces/IBranch.cs
+++ currencyExchangeTelegramBOT/Banks/01-Inerfaces/IBranch.cs
@@ -5,8 +5,8 @@ namespace Banks
     interface IBranch
     {
         [Key] public string Key { get; set; }
-        public string AddrLat { get; set; }
-        public string AddrRus { get; set; }
+        public string AdrLat { get; set; }
+        public string AdrRus { get; set; }
         public string[] Phones { get; set; }
     }
 }
--- currencyExchangeTelegramBOT/Banks/02-Classes/Branches.cs
+++ currencyExchangeTelegramBOT/Banks/02-Classes/Branches.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Banks._01_Inerfaces;
 using Banks._02_Classes;
 
 namespace Banks
@@ -7,17 +6,19 @@ namespace Banks
     public class Branches : IBranch
     {
         [Key] public string Key { get; set; }
-        public string AddrLat { get; set; }
-        public string AddrRus { get; set; }
+        public Bank BankName { get; set; }
+        public string AdrLat { get; set; }
+        public string AdrRus { get; set; }
         public string[] Phones { get; set; }
        
         public Branches() { }
 
-        public Branches(string key, string addrLat, string addrRus, string[] phones)
+        public Branches(string key, Bank bankName, string adrLat, string adrRus, string[] phones)
         {
             Key = key;
-            AddrLat = addrLat;
-            AddrRus = addrRus;
+            BankName = bankName;
+            AdrLat = adrLat;
+            AdrRus = adrRus;
             Phones = phones;
         }
     }
