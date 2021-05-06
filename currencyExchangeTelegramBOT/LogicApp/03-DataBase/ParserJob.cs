7b2f2a86cd7407504a92c5247413da3351ebc7b3
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-05-06 10:41:11 +0300

    создана Job

---
+++ currencyExchangeTelegramBOT/LogicApp/03-DataBase/ParserJob.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using FluentScheduler;
+using HtmlParse;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace LogicApp
+{
+    public class ParserJob : IJob
+    {
+        public void Execute() =>
+            new Parser().Start();
+    }
+
+    public class Parser
+    {
+
+
+        public void Start()
+        {
+            //Task
+
+            //Обновить словари (города, валюты)
+
+
+
+            //Отпарсить все валюты
+
+        }
+
+        private List<T> ParseData<T>(string partUrl)
+        {
+            using IWebDriver driver = new ChromeDriver
+            {
+                Url = @"https://m.select.by" + $"{partUrl}"
+            };
+            var resultData = new WebSiteData(driver).GeParseData<T>();
+            driver.Close();
+            return resultData;
+        }
+    }
+}
